a9a0f689b75252368a9a7ba9835a4be7a9b2c4bc
Pravalika <137316122+PravalikaReddyMaccha@users.noreply.github.com>
2025-04-14 11:24:52 +0530

    Add files via upload

---
+++ Practice2-BatBallGame/script.js
@@ -0,0 +1,54 @@
+
+const buttons = document.querySelectorAll('.bttn');
+let userValue=0;
+// const compValue=0;
+buttons.forEach(bttn=>{
+
+    bttn.addEventListener('click', function(){
+    const userChoice = bttn.textContent;
+    const compValue = Math.floor(Math.random() * 3) + 1;
+    console.log(userChoice+" "+compValue);
+    if(userChoice==='Bat'){
+       userValue=1;
+     }
+    else if(userChoice==='Ball'){
+        userValue=2;
+    }
+    else{
+        userValue=3;
+    }
+    playGame(compValue,userValue);
+});
+})
+function playGame(compValue,userValue){
+    if(compValue===userValue){
+        alert('It\'s Tie');
+    }
+    else if(compValue===1){
+        if(userValue===2){
+            alert('Computer won');
+        }
+        else{
+            alert('User won');
+        }
+    
+    }
+    else if(compValue===2){
+        if(userValue===3){
+            alert('Computer won');
+        }
+        else{
+            alert('User won');
+        }
+    }
+    else{
+        if(userValue===1){
+            alert('Computer won');
+        }
+        else{
+            alert('User won');
+        }
+    }
+}
+
+
